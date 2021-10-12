c1c8be624288aa0217f9053ec93954e017b32024
vrbalu <lukasvrbaa@seznam.cz>
2021-10-12 22:09:10 +0200

    Added FindByField function

--- app/services/dbService.go
+++ app/services/dbService.go
@@ -4,6 +4,7 @@ import (
 	"GIT/config"
 	"context"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
@@ -19,7 +20,7 @@ type DbServiceType struct {
 func init() {
 	DB.establishConnection()
 }
-func (db *DbServiceType) establishConnection() {
+func (ds *DbServiceType) establishConnection() {
 	clientOptions := options.Client().
 		ApplyURI(config.DBUrl)
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
@@ -28,46 +29,86 @@ func (db *DbServiceType) establishConnection() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	db.db = client.Database(config.DBName)
+	ds.db = client.Database(config.DBName)
 	return
 }
 
-func (db DbServiceType) InsertOne(coll string, data interface{}) (*mongo.InsertOneResult, error) {
+func (ds DbServiceType) InsertOne(coll string, data interface{}) (*mongo.InsertOneResult, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	one, err := db.db.Collection(coll).InsertOne(ctx, data)
+	one, err := ds.db.Collection(coll).InsertOne(ctx, data)
 	if err != nil {
 		return nil, err
 	}
 	return one, nil
 }
 
-func (db DbServiceType) GetOne(coll string, key string, value string, resultModel interface{}) (interface{}, error) {
+func (ds DbServiceType) GetOne(coll string, key string, value string, resultModel interface{}) (interface{}, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	filter := bson.D{{key, value}}
-	err := db.db.Collection(coll).FindOne(ctx, filter).Decode(resultModel)
+	err := ds.db.Collection(coll).FindOne(ctx, filter).Decode(resultModel)
 	if err != nil {
 		return nil, err
 	}
 	return resultModel, nil
 }
 
-func (db DbServiceType) DeleteOne(coll string, key string, value string) (*mongo.DeleteResult, error) {
+func (ds DbServiceType) FindByField(coll string, field string, value string) (interface{}, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	filter, err := buildFilter(field, value)
+	if err != nil {
+		defer cancel()
+		return nil, err
+	}
+	var result []bson.M
+	cursor, err := ds.db.Collection(coll).Find(ctx, filter)
+	defer func(cursor *mongo.Cursor, ctx context.Context) {
+		err = cursor.Close(ctx)
+	}(cursor, ctx)
+	if err != nil {
+		defer cancel()
+		return nil, err
+	}
+
+	for cursor.Next(ctx) {
+		var item bson.M
+		err = cursor.Decode(&item)
+		if err == nil {
+			result = append(result, item)
+		}
+	}
+	defer cancel()
+	return result, nil
+}
+
+func buildFilter(field string, value string) (bson.M, error) {
+	if field == "_id" {
+		objId, err := primitive.ObjectIDFromHex(value)
+		if err != nil {
+			return nil, err
+		}
+		return bson.M{field: objId}, nil
+	}
+	return bson.M{field: value}, nil
+}
+
+func (ds DbServiceType) DeleteOne(coll string, key string, value string) (*mongo.DeleteResult, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	filter := bson.D{{key, value}}
-	res, err := db.db.Collection(coll).DeleteOne(ctx, filter)
+	res, err := ds.db.Collection(coll).DeleteOne(ctx, filter)
 	if err != nil {
 		return nil, err
 	}
 	return res, nil
 }
 
-func (db DbServiceType) UpdateOneById(coll string, id string, updateValue interface{}) (*mongo.UpdateResult, error) {
+func (ds DbServiceType) UpdateOneById(coll string, id string, updateValue interface{}) (*mongo.UpdateResult, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	res, err := db.db.Collection(coll).UpdateByID(ctx, id, updateValue)
+	res, err := ds.db.Collection(coll).UpdateByID(ctx, id, updateValue)
 	if err != nil {
 		return nil, err
 	}
